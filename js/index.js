6de0c3157ef45a53482d3da64f3c78f3d75272b7
nmaswood <nmaswood@civisanalytics.com>
2018-05-19 15:59:39 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,174 @@
+import Vue from 'vue';
+import axios from 'axios';
+
+import 'bootstrap';
+
+import '../style/index.scss';
+
+var vm = new Vue({
+  el: '#vue-instance',
+  methods: {
+    processInformation: function() {
+
+      const values = {}
+
+      this.foodItems.forEach((x) => values[x.model] = x.value || 0)
+      this.configItems.forEach((x) => values[x.model] = x.value)
+
+      return values;
+    },
+
+    postInformation: function() {
+      const errors = this.validateData(this.form);
+
+      if (errors.length) {
+        console.log(errors);
+        return;
+      }
+
+      this.carrierItems = [];
+      this.foodItemsRes = [];
+      this.custom = [];
+
+      const data = this.processInformation();
+
+      const promise = axios.post('/submit', data);
+      
+      const that = this;
+
+      promise.then(function(res) {
+
+        const data = res.data;
+
+        console.log(data);
+
+        that.vendor = data.vendor;
+        that.price= data.price;
+
+        that.carrierItems.push({
+          'price': data.base_price_dict.USM,
+          'msg': data.base_price_dict.USM,
+          'carrier': 'USM'
+        });
+
+        that.carrierItems.push({
+          'price': data.base_price_dict.LS,
+          'carrier': 'LD'
+        });
+
+        const keys = ['cakePops', 'frenchMacarons', 'miniCupcakes', 'other', 'regularCupcakes', 'tiers'];
+        const display = ['Cake Pops', 'French Macarons', 'Mini Cupcakes', 'Other', 'Regular Cupcakes', 'Tiers'];
+
+        that.custom = data.food_item_dict.custom;
+
+        that.ldSum = data.food_item_dict.ld_sum;
+        that.usmSum = data.food_item_dict.usm_sum;
+
+        for (let i = 0; i < keys.length; i++) {
+          let item = data.food_item_dict.per_item[keys[i]];
+
+          if (!item._input) {
+            continue;
+          }
+
+          const status = item.status;
+          let msg = '';
+
+          if (status == 'LD_NULL' || status == 'USM_NULL' || status == 'LD_AND_USM_NULL') {
+          } else if (status == 'CUSTOM') {
+            msg = 'Price needs to be custom calculated';
+          }
+          that.foodItemsRes.push({
+            'name': display[i],
+            'usm': item.usm,
+            'ls': item.ld
+          })
+        }
+
+      });
+
+    },
+    validateData: function(formData) {
+      const errors = [];
+      //if (!/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(formData.zipCode)){
+      //errors.push('Invalid Zipcode');
+      //}
+
+      return errors;
+
+    }
+  },
+  data: function() {
+
+    const currentDateTime = new Date();
+    const asString = currentDateTime.toISOString();
+
+    return {
+      zipCode: '',
+      dateTime: '',
+      miniCupcakes: '',
+      regularCupcakes: '',
+      cakePops: '',
+      frenchMacarons: '',
+      tiers: '',
+      other: '',
+      carrierItems: [],
+      foodItemsRes: [],
+      vendor:'',
+      price :0,
+      custom: [],
+      ldSum: 0,
+      usmSum: 0,
+      configItems: [{
+          'model': 'zipCode',
+          'label': 'Enter your zipcode.',
+          'inputType': 'text',
+          'value': '60601'
+        },
+        {
+          'model': 'dateTime',
+          'label': 'Enter the date.',
+          'inputType': 'datetime-local',
+          'value': asString
+        }
+      ],
+      foodItems: [{
+          'model': 'miniCupcakes',
+          'displayName': 'Mini Cupcakes',
+          'hint': 'In dozens',
+          'value': 0
+        },
+        {
+          'model': 'regularCupcakes',
+          'displayName': 'Regular Cupcakes',
+          'hint': 'In dozens',
+          'value': 0
+        },
+        {
+          'model': 'cakePops',
+          'displayName': 'Cake Pops',
+          'hint': 'In dozens',
+          'value': 0
+        },
+        {
+          'model': 'frenchMacarons',
+          'displayName': 'French Macarons',
+          'hint': 'In dozens',
+          'value': 0
+        },
+        {
+          'model': 'tiers',
+          'displayName': 'Tiers',
+          'hint': '',
+          'value': 0
+        },
+        {
+          'model': 'other',
+          'displayName': 'Other',
+          'hint': '',
+          'value': 0
+        }
+      ]
+    }
+  }
+});
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const webpack = require('webpack');
+
+const config = {
+  entry: __dirname + '/js/index.js',
+  output: {
+    path: __dirname + '/asset_dist',
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.css'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      'axios$': 'axios/dist/axios.js'
+    }
+  },
+  module: {
+    rules: [{
+        test: /\.(s|)css$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+          }, {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: 'postcss-loader', // Run post css actions
+            options: {
+              plugins: function() { // post css plugins, can be exported to postcss.config.js
+                return [
+                  require('precss'),
+                  require('autoprefixer')
+                ];
+              }
+            }
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      }, {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {}
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      }
+    ]
+  }
+};
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
+
+module.exports = config;
